434d8010f49889a606ee2f0e44513228998f87a7
Sergei P. Vinogradov <serg.pvv@gmail.com>
2020-02-03 14:47:57 +0000

    add erorrs tests

--- __tests__/loadPage.test.js
+++ __tests__/loadPage.test.js
@@ -10,16 +10,20 @@ import loadPage from '../src';
 
 const buildFilepath = (filename) => path.join(__dirname, '__fixtures__', filename);
 
-const url = 'https://blah.blah';
+const site = 'https://go.ods';
 const pagePath = '/test/page';
-const pageName = 'blah-blah-test-page';
+const pageName = 'go-ods-test-page';
+const url = `${site}${pagePath}`;
+const wrongUrl = 'https://nonexist.ent';
 const resourcesDir = `${pageName}_files`;
 const resourcesPath = 'assets';
 let outputDir;
 
+nock.disableNetConnect();
+
 beforeEach(async () => {
   outputDir = await fs.mkdtemp(path.join(os.tmpdir(), path.sep));
-  nock(url)
+  nock(site)
     .get(pagePath)
     .replyWithFile(200, buildFilepath('helloworld'), { 'Content-Type': 'text/html' })
     .get(`/${resourcesPath}/style.css`)
@@ -30,27 +34,56 @@ beforeEach(async () => {
     .replyWithFile(200, buildFilepath('picture'), { 'Content-Type': 'image/jpg' });
 });
 
-it('download page', async () => {
-  const filename = await loadPage(`${url}${pagePath}`, outputDir);
-  const expectedFilename = path.join(outputDir, `${pageName}.html`);
-  expect(await fs.readFile(filename, 'utf-8')).not.toBeNull();
-  expect(filename).toBe(expectedFilename);
+describe('correct data', () => {
+  it('download page', async () => {
+    const filename = await loadPage(url, outputDir);
+    const expectedFilename = path.join(outputDir, `${pageName}.html`);
+    expect(await fs.readFile(filename, 'utf-8')).not.toBeNull();
+    expect(filename).toBe(expectedFilename);
+  });
+
+  it('download resources', async () => {
+    await loadPage(url, outputDir);
+    [['css', 'style', 'utf-8'], ['js', 'script', 'utf-8']]
+      .forEach(async ([type, name, encoding]) => {
+        const resFilename = path.join(outputDir, resourcesDir, `${resourcesPath}-${name}.${type}`);
+        expect(await fs.readFile(resFilename, encoding))
+          .toBe(await fs.readFile(buildFilepath(name), encoding));
+      });
+  });
+
+  it('download binary resource', async () => {
+    await loadPage(url, outputDir);
+    const { size: expected } = await fs.stat(buildFilepath('picture'));
+    const resFilename = path.join(outputDir, resourcesDir, `${resourcesPath}-picture.jpg`);
+    const { size: actual } = await fs.stat(resFilename);
+    expect(actual).toBe(expected);
+  });
 });
 
-it('download resources', async () => {
-  await loadPage(`${url}${pagePath}`, outputDir);
-  [['css', 'style', 'utf-8'], ['js', 'script', 'utf-8']]
-    .forEach(async ([type, name, encoding]) => {
-      const resFilename = path.join(outputDir, resourcesDir, `${resourcesPath}-${name}.${type}`);
-      expect(await fs.readFile(resFilename, encoding))
-        .toBe(await fs.readFile(buildFilepath(name), encoding));
-    });
-});
-
-it('download binary resource', async () => {
-  await loadPage(`${url}${pagePath}`, outputDir);
-  const { size: expected } = await fs.stat(buildFilepath('picture'));
-  const resFilename = path.join(outputDir, resourcesDir, `${resourcesPath}-picture.jpg`);
-  const { size: actual } = await fs.stat(resFilename);
-  expect(actual).toBe(expected);
+describe('test errors', () => {
+  nock(wrongUrl)
+    .get('/')
+    .replyWithError({
+      message: 'getaddrinfo ENOTFOUND',
+      code: 'ENOTFOUND',
+    })
+    .get(pagePath)
+    .reply(404);
+  it('nonexistent hostname', async () => {
+    await expect(loadPage(`${wrongUrl}/`, outputDir))
+      .rejects.toThrowErrorMatchingSnapshot();
+  });
+  it('nonexistent resource', async () => {
+    await expect(loadPage(`${wrongUrl}${pagePath}`, outputDir))
+      .rejects.toThrowErrorMatchingSnapshot();
+  });
+  it('nonexistent output directory', async () => {
+    await expect(loadPage(url, '/nonexist'))
+      .rejects.toThrowErrorMatchingSnapshot();
+  });
+  it('permission denied', async () => {
+    await expect(loadPage(url, '/home'))
+      .rejects.toThrowErrorMatchingSnapshot();
+  });
 });
